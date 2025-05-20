5e5d164b204837fac787dd944771d01eb629c16c
B-efore <ljw010512@naver.com>
2025-05-20 20:52:01 +0900

    feat: 임시 Home 버튼 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import Header from "../components/header/Header";
+import "./Home.css";
 
 const Home = () => {
     return(
         <div>
             <Header/>
+            <div className = "home-body">
+                <button className="start-btn">MyLog 시작하기</button>
+                <button className="mylog-btn">나의 로그</button>
+            </div>
         </div>
     );
 };
