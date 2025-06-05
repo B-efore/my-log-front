ec9a99868441ec632e7294d581d4e98cbf970ce0
B-efore <ljw010512@naver.com>
2025-06-05 23:36:18 +0900

    feat: Header 데이터 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,13 @@ import { useAuth } from '../../context/AuthContext';
 import HeaderDropdown from './HeaderDropDown';
 import './Header.css';
 
-const Header = ({ name, leftChild, rightChild }) => {
+const Header = ({ leftChild, rightChild }) => {
 
     const navigate = useNavigate();
     const goHome = () => navigate("/");
     const goLogin = () => navigate("/login");
     const goWrite = () => navigate("/write");
-    const { isLoggedIn, userImage, setLogout } = useAuth();
+    const { userId, username, isLoggedIn, userImage, setLogout } = useAuth();
 
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const toggleDropdown = () => setIsDropdownOpen(prev => !prev);
@@ -21,47 +21,61 @@ const Header = ({ name, leftChild, rightChild }) => {
         setLogout();
         navigate("/");
         setIsDropdownOpen(false);
-      };
+    };
 
     return (
         <header className="header">
-            <div className="header-left" onClick={goHome}>
-                <img className="logo" src={getLogoImage()} />
-                <span className="blog-name">{name}</span>
-            </div>
+            <div className="header-bar">
+                <div className="header-left" onClick={goHome}>
+                    <img className="header-logo" src={getLogoImage()} />
+                    <span className="header-username">{username}</span>
+                </div>
 
-            <div className="header-right">
-                {rightChild ? rightChild : (
-                    <>
-                        <FiSearch className="icon-button" title="검색" />
-                        {isLoggedIn ? (
-                            <>
-                                <FiEdit3 className="icon-button" title="편집" onClick={goWrite} />
-                                <div className="profile-wrapper">
-                                    <img
-                                        src={userImage}
-                                        alt="profile"
-                                        className="profile-image"
-                                        onClick={toggleDropdown}
-                                    />
-                                    {isDropdownOpen && (
-                                        <HeaderDropdown
-                                            onLogout={handleLogout}
-                                            onClose={() => setIsDropdownOpen(false)}
-                                            onNavigate={(path) => {
-                                                navigate(path);
-                                                setIsDropdownOpen(false);
-                                            }}
+                <div className="header-right">
+                    {rightChild ? rightChild : (
+                        <>
+                            <FiSearch className="icon-button" title="검색" />
+                            {isLoggedIn ? (
+                                <>
+                                    <FiEdit3 className="icon-button" title="편집" onClick={goWrite} />
+                                    <div className="profile-wrapper">
+                                        <img
+                                            src={userImage}
+                                            alt="profile"
+                                            className="header-profile-image"
+                                            onClick={toggleDropdown}
                                         />
-                                    )}
-                                </div>
-                            </>
-                        ) : (
-                            <button className="header-login-button" onClick={goLogin}>로그인</button>
-                        )}
-                    </>
-                )}
+                                        {isDropdownOpen && (
+                                            <HeaderDropdown
+                                                userId={userId}
+                                                onLogout={handleLogout}
+                                                onClose={() => setIsDropdownOpen(false)}
+                                                onNavigate={(path) => {
+                                                    navigate(path);
+                                                    setIsDropdownOpen(false);
+                                                }}
+                                            />
+                                        )}
+                                    </div>
+                                </>
+                            ) : (
+                                <button className="header-login-button" onClick={goLogin}>로그인</button>
+                            )}
+                        </>
+                    )}
+                </div>
             </div>
+            {/* <div className="header-bottom">
+                <div className="header-nav">
+                    <nav>
+                        <ul>
+                            <li>홈</li>
+                            <li>게시글</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div> */}
+
         </header>
     );
 }
--- src/components/Header/HeaderDropDown.jsx
+++ src/components/Header/HeaderDropDown.jsx
@@ -2,13 +2,13 @@ import { useRef } from "react";
 import { useOutsideClick } from "../../hooks/useOutsideClick";
 import "./HeaderDropdown.css";
 
-const HeaderDropdown = ({ onClose, onNavigate, onLogout }) => {
+const HeaderDropdown = ({ userId, onClose, onNavigate, onLogout }) => {
     const ref = useRef(null);
     useOutsideClick(ref, onClose);
 
     return (
         <div className="header-dropdown" ref={ref}>
-            <div className="header-dropdown__item" onClick={() => onNavigate("/")}>블로그</div>
+            <div className="header-dropdown__item" onClick={() => onNavigate(`/${userId}`)}>블로그</div>
             <div className="header-dropdown__item" onClick={() => onNavigate("/settings")}>설정</div>
             <div className="header-dropdown__item" onClick={() => {
                 onLogout();
